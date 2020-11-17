9251c5bafc9c9eb2b6e7fd47691f834dbf4dc680
Hashmi, Yasub <yas.hashmi@gmail.com>
2020-11-17 15:52:07 +0530

    Calling API from Cucumber Steps

--- src/test/java/stepDefinition/EnergyChargesCalculatorSteps.java
+++ src/test/java/stepDefinition/EnergyChargesCalculatorSteps.java
@@ -1,22 +1,38 @@
 package stepDefinition;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.freshCode.utilityChargesCalculator.api.*;
-import com.freshCode.utilityChargesCalculator.repository.EnergyChargesBaseRateProviderImpl;
-import com.freshCode.utilityChargesCalculator.service.EnergyChargesCalculator;
 
 import io.cucumber.java.en.*;
 import org.junit.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class EnergyChargesCalculatorSteps {
 
 	EnergyChargesController chargesController;
-	double charges = 0.0;
+
+	ResponseEntity<String> response;
+	String baseUrl = "http://localhost:8060/api/v1/energy/energyCharges?unitsConsumed=20";
 
 	@Given("I have consumed {int} units in current month")
-	public void i_have_consumed_units_in_current_month(Integer consumedUnits) {
-		EnergyChargesCalculator energyChargesCalculator = new EnergyChargesCalculator(new EnergyChargesBaseRateProviderImpl());
-		chargesController = new EnergyChargesController(energyChargesCalculator);
-		charges = chargesController.calculateCharges(consumedUnits);
+	public void i_have_consumed_units_in_current_month(Integer consumedUnits) throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+		URI uri = new URI(baseUrl);
+
+		response = restTemplate.getForEntity(uri, String.class);
+		Assert.assertNotNull(response);
+
+		Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
 	}
 
 	@When("I submit the input units")
@@ -25,8 +41,12 @@ public class EnergyChargesCalculatorSteps {
 	}
 
 	@Then("I should be able to view total energy charges as {double}")
-	public void i_should_be_able_to_view_total_energy_charges_as(double int1) {
-		Assert.assertEquals(int1, charges, 0.0);
+	public void i_should_be_able_to_view_total_energy_charges_as(double int1)
+			throws JsonMappingException, JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(response.getBody());
+		
+		assertEquals(root.asDouble(), 81.0, 0.0);
 	}
 
 }
