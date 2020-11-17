90bf12d56046614952ea448f090b7fb299124889
Hashmi, Yasub <yas.hashmi@gmail.com>
2020-11-17 17:36:42 +0530

    removed the hard coded values, moved to query param and got values from feature file

--- src/test/java/stepDefinition/EnergyChargesCalculatorSteps.java
+++ src/test/java/stepDefinition/EnergyChargesCalculatorSteps.java
@@ -2,7 +2,6 @@ package stepDefinition;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -16,20 +15,24 @@ import org.junit.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class EnergyChargesCalculatorSteps {
 
 	EnergyChargesController chargesController;
 
 	ResponseEntity<String> response;
-	String baseUrl = "http://localhost:8060/api/v1/energy/energyCharges?unitsConsumed=20";
+	String baseUrl = "http://localhost:8060/api/v1/energy/energyCharges";
 
 	@Given("I have consumed {int} units in current month")
 	public void i_have_consumed_units_in_current_month(Integer consumedUnits) throws URISyntaxException {
 		RestTemplate restTemplate = new RestTemplate();
-		URI uri = new URI(baseUrl);
+		// URI uri = new URI(baseUrl);
 
-		response = restTemplate.getForEntity(uri, String.class);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParam("unitsConsumed",
+				consumedUnits.toString());
+		response = restTemplate.getForEntity(builder.build().encode().toUri(), String.class);
+
 		Assert.assertNotNull(response);
 
 		Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
@@ -41,12 +44,12 @@ public class EnergyChargesCalculatorSteps {
 	}
 
 	@Then("I should be able to view total energy charges as {double}")
-	public void i_should_be_able_to_view_total_energy_charges_as(double int1)
+	public void i_should_be_able_to_view_total_energy_charges_as(double expectedCharges)
 			throws JsonMappingException, JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode root = mapper.readTree(response.getBody());
-		
-		assertEquals(root.asDouble(), 81.0, 0.0);
+
+		assertEquals(expectedCharges, root.asDouble(), 0.0);
 	}
 
 }
