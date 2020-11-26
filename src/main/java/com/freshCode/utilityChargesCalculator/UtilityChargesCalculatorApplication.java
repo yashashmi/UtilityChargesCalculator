f87c98c7cab722e7bf0c07f1c92b0bab1571d641
Hashmi, Yasub <yas.hashmi@gmail.com>
2020-11-26 12:19:02 +0530

    Changing from jar to war

--- src/main/java/com/freshCode/utilityChargesCalculator/UtilityChargesCalculatorApplication.java
+++ src/main/java/com/freshCode/utilityChargesCalculator/UtilityChargesCalculatorApplication.java
@@ -2,9 +2,16 @@ package com.freshCode.utilityChargesCalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UtilityChargesCalculatorApplication {
+public class UtilityChargesCalculatorApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UtilityChargesCalculatorApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UtilityChargesCalculatorApplication.class, args);
