8c5c8a31248b493ac796d15c3348db6f28fc9558
Yasub Hashmi <yas.hashmi@gmail.com>
2021-07-09 11:38:28 +0530

    added selenium tests code

--- src/test/java/uIStepDefinitions/EnergyChargesCalculatorUISteps.java
+++ src/test/java/uIStepDefinitions/EnergyChargesCalculatorUISteps.java
@@ -2,59 +2,70 @@ package uIStepDefinitions;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import io.cucumber.java.en.*;
 import uIStepDefinitions.utility.BaseUtil;
 
 public class EnergyChargesCalculatorUISteps {
 
-
     BaseUtil baseUtil;
-	String url = "http://localhost:8080";
+    String url = "http://localhost:8081";
 
-	public EnergyChargesCalculatorUISteps(BaseUtil baseUtil) {
-		this.baseUtil = baseUtil;
-		baseUtil.Driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-    
+    public EnergyChargesCalculatorUISteps(BaseUtil baseUtil) {
+        this.baseUtil = baseUtil;
+        baseUtil.Driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
     @Given("I have consumed {int} units in current month")
-    public void i_have_consumed_units_in_current_month(Integer int1) {
+    public void i_have_consumed_units_in_current_month(Integer consumedUnits) {
         baseUtil.Driver.navigate().to(url);
-		baseUtil.Driver.manage().window().maximize();
+        baseUtil.Driver.manage().window().maximize();
+        WebElement unitTextBox = baseUtil.Driver.findElement(By.id("unitsConsumed"));
+
+        unitTextBox.sendKeys(consumedUnits.toString());
+
     }
 
     @When("I submit the input units")
     public void i_submit_the_input_units() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        baseUtil.Driver.findElement(By.id("submit")).click();
     }
 
     @Then("I should see energy charges for first slab as {double}")
-    public void i_should_see_energy_charges_for_first_slab_as(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_see_energy_charges_for_first_slab_as(Double expectedCharges) {
+        WebElement chargesElement = baseUtil.Driver.findElement(By.xpath("/html/body/div/section/div[3]/table/tbody/tr[4]/td[4]"));
+        Double actualCharges = Double.parseDouble(chargesElement.getText());
+        Assert.assertEquals(expectedCharges, actualCharges, 0.01);
     }
 
     @Then("I should see energy charges for second slab as {double}")
-    public void i_should_see_energy_charges_for_second_slab_as(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_see_energy_charges_for_second_slab_as(Double expectedCharges) {
+        WebElement chargesElement = baseUtil.Driver.findElement(By.xpath("/html/body/div/section/div[3]/table/tbody/tr[5]/td[4]"));
+        Double actualCharges = Double.parseDouble(chargesElement.getText());
+        Assert.assertEquals(expectedCharges, actualCharges, 0.01);
     }
 
     @Then("I should see energy charges for third slab as {double}")
-    public void i_should_see_energy_charges_for_third_slab_as(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_see_energy_charges_for_third_slab_as(Double expectedCharges) {
+        WebElement chargesElement = baseUtil.Driver.findElement(By.xpath("/html/body/div/section/div[3]/table/tbody/tr[6]/td[4]"));
+        Double actualCharges = Double.parseDouble(chargesElement.getText());
+        Assert.assertEquals(expectedCharges, actualCharges, 0.01);
     }
 
     @Then("I should see energy charges for last slab as {double}")
-    public void i_should_see_energy_charges_for_last_slab_as(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_see_energy_charges_for_last_slab_as(Double expectedCharges) {
+        WebElement chargesElement = baseUtil.Driver.findElement(By.xpath("/html/body/div/section/div[3]/table/tbody/tr[7]/td[4]"));
+        Double actualCharges = Double.parseDouble(chargesElement.getText());
+        Assert.assertEquals(expectedCharges, actualCharges, 0.01);
     }
 
     @Then("I should see total energy charges as {double}")
-    public void i_should_see_total_energy_charges_as(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_see_total_energy_charges_as(Double expectedCharges) {
+        WebElement chargesElement = baseUtil.Driver.findElement(By.xpath("/html/body/div/section/div[2]/table/tbody/tr[1]/td"));
+        Double actualCharges = Double.parseDouble(chargesElement.getText());
+        Assert.assertEquals(expectedCharges, actualCharges, 0.01);
     }
 }
--- src/test/java/uIStepDefinitions/Hook.java
+++ src/test/java/uIStepDefinitions/Hook.java
@@ -4,8 +4,8 @@ package uIStepDefinitions;
 import io.cucumber.java.*;
 import uIStepDefinitions.utility.BaseUtil;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hook extends BaseUtil {
 	private final BaseUtil base;
@@ -16,14 +16,11 @@ public class Hook extends BaseUtil {
 
 	@Before
 	public void InitTest() {
-		System.out.println("Test Init: Before");
-		// System.setProperty("webdriver.chrome.driver",
-		// "C:\\Users\\yasub.hashmi\\Drivers\\chromedriver_win32//chromedriver.exe");
-		// base.Driver= new ChromeDriver();
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\yasub.hashmi\\Drivers\\geckodriver.exe");
-		FirefoxOptions options = new FirefoxOptions();
-		options.setHeadless(false);
-		base.Driver = new FirefoxDriver(options);
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\yasub.hashmi\\Drivers\\chromedriver_win32\\chromedriver.exe");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                
+                base.Driver = new ChromeDriver(chromeOptions);
 
 	}
 	
